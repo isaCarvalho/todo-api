08a3a20fe4a66295a3c66b128bae44d8d3af584f
isaCarvalho <isabelacarvalhocf@hotmail.com>
2022-08-18 18:12:41 -0300

    Add test

---
+++ src/test/kotlin/com/demuxer/todoapi/repository/SubTaskRepositoryTest.kt
@@ -0,0 +1,55 @@
+package com.demuxer.todoapi.repository
+
+import com.demuxer.todoapi.entity.SubTaskEntity
+import com.demuxer.todoapi.entity.TaskEntity
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import java.util.*
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class SubTaskRepositoryTest {
+
+    @Autowired
+    private lateinit var subTaskRepository: SubTaskRepository
+
+    @Autowired
+    private lateinit var taskRepository: TaskRepository
+
+    // task
+    private val taskId = UUID.randomUUID()
+    private val taskDescription = "This is a task description"
+    private val startDate = Date()
+    private val endDate = Date()
+    private val task = TaskEntity(taskId, taskDescription, startDate, endDate)
+
+    // first subtask
+    private val firstSubTaskId = UUID.randomUUID()
+    private val firstDescription = "This is the first subsection"
+    private val firstSubTask = SubTaskEntity(firstSubTaskId, firstDescription, task)
+
+    // second subtask
+    private val secondSubTaskId = UUID.randomUUID()
+    private val secondDescription = "This is the first subsection"
+    private val secondSubTask = SubTaskEntity(secondSubTaskId, secondDescription, task)
+
+    @BeforeAll
+    fun setup() {
+        taskRepository.save(task)
+        subTaskRepository.save(firstSubTask)
+        subTaskRepository.save(secondSubTask)
+    }
+
+    @Test
+    fun getAllTest() {
+        // when
+        val subTasks = subTaskRepository.findAll()
+
+        // then
+        Assertions.assertEquals(2, subTasks.size)
+    }
+}
