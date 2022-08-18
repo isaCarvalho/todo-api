38608446d723a30ff320d4d3bb727c49f0edf30b
isaCarvalho <isabelacarvalhocf@hotmail.com>
2022-08-18 13:33:00 -0300

    Add methods to controller

--- src/main/kotlin/com/demuxer/todoapi/controller/SubTaskController.kt
+++ src/main/kotlin/com/demuxer/todoapi/controller/SubTaskController.kt
@@ -5,9 +5,8 @@ import com.demuxer.todoapi.service.SubTaskService
 import com.demuxer.todoapi.util.Response
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 class SubTaskController(
@@ -18,5 +17,28 @@ class SubTaskController(
     @ResponseStatus(HttpStatus.OK)
     fun getAll() : Response<List<SubTaskDTO>> {
         return Response(subTaskService.getAll())
+    }
+
+    @GetMapping("/subtask/{id}")
+    fun getById(@PathVariable id: UUID) : Response<SubTaskDTO> {
+        return Response(subTaskService.getById(id))
+    }
+
+    @PostMapping("/")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create(@RequestBody subTask: SubTaskDTO) : Response<SubTaskDTO> {
+        return Response(subTaskService.create(subTask))
+    }
+
+    @PutMapping("/")
+    @ResponseStatus(HttpStatus.OK)
+    fun update(@RequestBody subTask: SubTaskDTO) : Response<SubTaskDTO> {
+        return Response(subTaskService.update(subTask))
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun delete(@PathVariable id: UUID) {
+        subTaskService.delete(id)
     }
 }
